8ec7cda19efab8ed13668b56e1033cc01fb49669
boxulavm <aa_bogdan@yahoo.com>
2020-12-28 17:18:28 +0100

    Description added

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -7,11 +7,19 @@ const Header = ({openModal}) => {
     }
 
     return (
+        <>
+        <div className="row mb-5">
+            <div className="col-sm-12">
+                <p>Data from <a href="https://jsonplaceholder.typicode.com/" rel="noreferrer" target="_blank">jsonplaceholder.typicode.com</a></p>
+                <p>Free to use fake online REST API for testing and prototyping.</p>
+            </div>
+        </div>
         <div className="row mb-4">
             <div className="col-sm-6 mb-1">
                 <button onClick={openModalF} className="btn btn-primary px-5"><i className="fas fa-plus"></i> Add post</button>
             </div>
         </div>
+        </>
     )
 }
 
